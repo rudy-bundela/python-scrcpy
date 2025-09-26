ded67abde374fcb2b0a259a4de8d87cf545eca5f
Rudy <rudranujbundella@gmail.com>
2025-09-26 05:47:12 +0000

    cleaning up

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ffmpeg -r 30 -f lavfi -i testsrc -vf scale=1280:960 -vcodec libx264 -profile:v baseline -pix_fmt yuv420p -f flv rtmp://localhost/live/test
-adb connect 192.168.0.165:42667
-scrcpy -ra.mp4 --video-codec=h264 --video-source=camera -b 5000000 --camera-high-speed --camera-size=1280x720 --video-buffer=250 --camera-fps=120 --no-playback --no-window --no-control --audio-codec=aac
-
-scrcpy -ra.mp4 --video-codec=h264 --video-source=camera -b 5000000 --camera-high-speed --camera-size=1280x720 --camera-fps=120 --no-playback --no-window --no-control --audio-codec=aac
